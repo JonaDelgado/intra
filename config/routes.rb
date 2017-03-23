1334468cc885dce78b97cdbe4771c39e844a14c1
JonaDelgado <jonathan.delgado25@gmail.com>
2017-03-23 12:42:26 -0300

    Esta es la intranet de un callcenter

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :accounts
+  resources :posts
+
+  
+  root 'posts#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
